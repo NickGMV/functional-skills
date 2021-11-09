e5abf9e8f3f559fd48563fc0027b3c184de20413
NickGMV <78032114+NickGMV@users.noreply.github.com>
2021-11-09 17:16:56 +0000

    webpage update

---
+++ assets/website_generator_first_ideas.py
@@ -0,0 +1,15 @@
+import os
+
+
+# create function ot map a set folder tree
+
+
+
+# use map of tree to infill element paths for webpage
+
+# select appropriate HTML section for each element
+
+
+pathname = ''
+content_section = f'some HTML {pathname}'
+links_section = f'some HTML {pathnames}'
