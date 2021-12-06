9ab9a7771ee62a08e50206d0676aded6014acb93
NickGMV <78032114+NickGMV@users.noreply.github.com>
2021-12-06 15:37:01 +0000

    major update, site structure starting to develop
    refactor will be needed soon but general structure is coming along.
    
    Vids, pdfs and lesson plans are working

--- website_generator_first_ideas.py
+++ website_generator_first_ideas.py
@@ -5,7 +5,7 @@
 
 
 ######################
-import numpy
+
 import pandas as pd
 import os
 base_path = '/Users/Nick/Documents/GitHub/functional-skills/content'
@@ -20,30 +20,53 @@ print(arr)
 
 # next to use  pre written file structure to start generating the main page
 for part in arr:
+    part_name = f'{part}.html'
     sections = os.listdir( f'{base_path}/{part}')
     print(sections)
+    with open(part_name, "w") as g:
+        g.write('<!DOCTYPE html>\n<html lang ="en">\n<link href="fs_styles.css" type= "text/css" rel="stylesheet"/>\n<!--comment -->\n\n\n')
+        g.write(f'<head>\n <title>{part} lessons </title>\n</head> \n<body>\n<hr>\n\n')
+        g.write('<div class = "buffer"></div>\n\n')
+        g.write(f'<a href = "./{part}.html"> go back a level </a>')
+        for section in sections:
+            g.write(f'<a href = "./{part}_{section}_lessons.html"> go to lesson page </a>')
+
+    #this loop will be refactored but for now it will iterate through the sections and buid each lesson page
     for section in sections:
         page_name = f"{part}_{section}_lessons.html"
+
+        #writing the opening html code for a lesson page
         with open(page_name,'w') as f:
             f.write('<!DOCTYPE html>\n<html lang ="en">\n<link href="fs_styles.css" type= "text/css" rel="stylesheet"/>\n<!--comment -->\n\n\n')
             f.write(f'<head>\n <title>{part}{section} lessons </title>\n</head> \n<body>\n<hr>\n\n')
+            f.write('<div class = "buffer"></div>\n\n')
+            f.write(f'<a href = "./{part}"> go back a level </a>')
+
 
             # now lesson page has been established find resource types.
             
             # find videos and load them
             try:
-                structure = os.listdir(f'{base_path}/{part}/{section}/instruction sections')
+                structure = os.listdir(f'{base_path}/{part}/{section}/instructionsections')
                 print(f'found session structure for {part}{section}')
-                lesson = pd.read_csv(structure)
-                for row in lesson[['type','content']]:
+                #print(structure)
+                structure = [file for file in structure if file.endswith('csv') ]
+                #print(structure)
+                lesson = pd.read_csv(f'{base_path}/{part}/{section}/instructionsections/{structure[0]}')
+                print('pandas worked')
+                for index, row in lesson.iterrows():
+                    print('df loaded')
                     if row['type'] == 'heading':
                         f.write(f'<h2>{row["content"]}</h2>')
+
                     elif row['type'] == 'text':
                         f.write(f'<p>{row["content"]}</p>')
+#<div style="position: relative; padding-bottom: 56.25%; height: 0;"><iframe src="https://www.loom.com/embed/de00a87a28864b7784607282e65f40dd" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe>
                     elif row['type']== 'vid':
-                        f.write(f'<iframe src = {row["content"]}></iframe>')
+                        f.write(f'<iframe src = {row["content"]} webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>')
+
                     else:
-                        print('content type npt recognised check your planning document for errors')
+                        print('content type not recognised check your planning document for errors')
             except:
                 print('there appears to be no planning doc for this section')
 
