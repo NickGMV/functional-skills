c0141e3399282111d0daec4406488f7cb4b7dc9f
NickGMV <78032114+NickGMV@users.noreply.github.com>
2021-12-30 17:07:43 +0000

    further development
    style sheets now load in so all pages can be customised via one single file.
    
    js needs to be added for any custom functionality
    
    iterations can now generate "over one another meaning deletion isn't required before compiling.
    
    Excel plans generate webpages mostly accurately, some work on pdfs required.

--- website_generator_first_ideas.py
+++ website_generator_first_ideas.py
@@ -1,28 +1,75 @@
 
 # Recycle file walking system to try and generate functional skills site
-
-
-
-
 ######################
 
 import pandas as pd
 import os
-base_path = '/Users/Nick/Documents/GitHub/functional-skills/content'
-#course_parts = []
+import shutil
 
-# load all parts
+# find the content folders from which to generate the site
+
+base_path = './content'
+
+
+#create a universal parent directory for all generated content
+# Directory
+
+try:
+    main_directory = "Functional skills maths"
+    # Parent Directory path
+    parent_directory = "./"
+    # Path
+    path = os.path.join(parent_directory, main_directory)
+    #Create the directory
+    os.mkdir(path)
+except:
+    go_ahead = input('the content directory has already been converted to a website! \n \
+    if you wish to over write this version enter any key execpt "n"')
+    if(go_ahead.lower() != 'n'):
+        #delete current site design to replace it
+        try:
+            shutil.rmtree(path)
+        except OSError as e:
+            print("Error: %s - %s." % (e.filename, e.strerror))
+    
+        main_directory = "Functional skills maths"
+        # Parent Directory path
+        parent_directory = "./"
+        # Path
+        path = os.path.join(parent_directory, main_directory)
+        #Create the directory
+        os.mkdir(path)
+
+    else:
+        # terminate and produce an error report
+        print('some error ')
+
+#copy style and js resources so that they can be used by all pages.
+
+
+original = './fs_styles.css'
+target = f'{path}/fs_styles.css'
+
+shutil.copyfile(original, target)
+
+
+#find all the sub-parts (content sections/courses) load all parts and generate navigation pages
 level1 = os.listdir(base_path)
-print(level1)
+print(f' content at the first level is {level1}\n\n\n')
 
-with open("main_page.html" ,"w") as h:
-    h.write('<!DOCTYPE html>\n<html lang ="en">\n<link href="fs_styles.css" type= "text/css" rel="stylesheet"/>\n<!--comment -->\n\n\n')
+
+with open(f"{parent_directory}/{main_directory}/main_page.html" ,"w") as h:
+    
+    # h refers to the main nav page being written by python in this loop
+    h.write('<!DOCTYPE html>\n<html lang ="en">\n<link href="./fs_styles.css" type= "text/css" rel="stylesheet"/>\n<!--comment -->\n\n\n')
     h.write(f'<head>\n <title>main_page </title>\n</head> \n<body>\n<hr>\n\n')
     h.write('<div class = "buffer"></div>\n\n')
     
-    for part in level1:
-        h.write(f'<a href = "./{part}.html"> go to part </a>')
 
+    for part in level1:
+        h.write(f'<a href = "./{part}/{part}.html"> go to part </a>')
+        os.mkdir(f'./{main_directory}/{part}')
+        
 
 
 print(level1)
@@ -30,26 +77,29 @@ print(level1)
 
 
 
-# next to use  pre written file structure to start generating the main page
+# generate actual lesson content now navigation pages are completed
 for part in level1:
     part_name = f'{part}.html'
+    print(part)
     sections = os.listdir( f'{base_path}/{part}')
-    print(sections)
-    with open(part_name, "w") as g:
-        g.write('<!DOCTYPE html>\n<html lang ="en">\n<link href="fs_styles.css" type= "text/css" rel="stylesheet"/>\n<!--comment -->\n\n\n')
+    print(f' sections in {part} are {sections}')
+
+
+    with open(f'./{main_directory}/{part}/{part_name}', "w") as g:
+        g.write('<!DOCTYPE html>\n<html lang ="en">\n<link href="../fs_styles.css" type= "text/css" rel="stylesheet"/>\n<!--comment -->\n\n\n')
         g.write(f'<head>\n <title>{part} lessons </title>\n</head> \n<body>\n<hr>\n\n')
         g.write('<div class = "buffer"></div>\n\n')
-        g.write(f'<a href = "./main_page.html"> go back a level </a>')
+        g.write(f'<a href = "../main_page.html"> go back a level </a>')
         for section in sections:
             g.write(f'<a href = "./{part}_{section}_lessons.html"> go to lesson page </a>')
 
-    #this loop will be refactored but for now it will iterate through the sections and buid each lesson page
+    #this loop will be refactored but for now it will iterate through the sections and build each lesson page
     for section in sections:
-        page_name = f"{part}_{section}_lessons.html"
+        page_name = f"./{main_directory}/{part}/{part}_{section}_lessons.html"
 
         #writing the opening html code for a lesson page
         with open(page_name,'w') as f:
-            f.write('<!DOCTYPE html>\n<html lang ="en">\n<link href="fs_styles.css" type= "text/css" rel="stylesheet"/>\n<!--comment -->\n\n\n')
+            f.write(f'<!DOCTYPE html>\n<html lang ="en">\n<link href="../fs_styles.css" type= "text/css" rel="stylesheet"/>\n<!--comment -->\n\n\n')
             f.write(f'<head>\n <title>{part}{section} lessons </title>\n</head> \n<body>\n<hr>\n\n')
             f.write('<div class = "buffer"></div>\n\n')
             f.write(f'<a href = "./{part}.html"> go back a level </a>')
@@ -96,7 +146,7 @@ for part in level1:
                 print(pp)
                 for pdf in pp:
                     print(pdf)
-                    f.write(f'<iframe class="pdf coached" src="./content/{part}/{section}/pdfs/{pdf}"  type="application/pdf" allow="fullscreen"></iframe>\n')
+                    f.write(f'<iframe class="pdf coached" src="../../content/{part}/{section}/pdfs/{pdf}"  type="application/pdf" allow="fullscreen"></iframe>\n')
                 #for pdf in pdfs:
                 #f.write(f'<embed class="pdf coached" src="{pdf}" type="application/pdf">')
             except:
