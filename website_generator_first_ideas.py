97de3271f3721c4c25dd810314af06e7c5cb8a72
NickGMV <78032114+NickGMV@users.noreply.github.com>
2022-02-02 10:41:32 +0000

    prep for demo meeting

--- website_generator_first_ideas.py
+++ website_generator_first_ideas.py
@@ -1,5 +1,7 @@
 
 # Recycle file walking system to try and generate functional skills site
+
+issue_count = 0
 ######################
 def banners(banner1,banner2,webpage):
     
@@ -9,7 +11,7 @@ def banners(banner1,banner2,webpage):
     g = open(banner2, 'r')
         
     for line in f:
-        print(line)
+        #print(line)
         h.write(line)
     #h.close()
 
@@ -22,20 +24,37 @@ def banners(banner1,banner2,webpage):
     f.close()
     g.close()
 
+def initsession():
+    e = open('error.txt','a')
+    time = datetime.now()
+    e.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n')
+    e.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n')
+    e.write(f'New session started at -- {time} \n\n')
+    e.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n')
+    e.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n\n')
+
+    f = open('success.txt','a')
+    f.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n')
+    f.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n')
+    f.write(f'New session started at -- {time} \n\n')
+    f.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n')
+    f.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n\n')
 
 def report_issue(x):
     #send reports to a txt file  in the root directory
     e = open('error.txt','a')
     time = datetime.now()
-    e.write(f'reported error at {time}')
+    e.write(f'reported error at {time}\n')
     e.write(f'{x}\n\n')
+    global issue_count 
+    issue_count +=1
 
 
 def report_success(x):
     #send reports to a txt file  in the root directory
     e = open('success.txt','a')
     time = datetime.now()
-    e.write(f'process successful at {time}')
+    e.write(f'process successful at {time}\n')
     e.write(f'{x}\n\n')
 
 
@@ -43,6 +62,7 @@ def report_success(x):
 import pandas as pd
 import os
 import shutil
+import openpyxl
 from datetime import datetime
 
 # find the content folders from which to generate the site
@@ -52,6 +72,7 @@ base_path = './content'
 
 #create a universal parent directory for all generated content
 # Directory
+initsession()
 
 try:
     main_directory = "Functional skills maths"
@@ -71,7 +92,8 @@ except:
             shutil.rmtree(path)
         except OSError as e:
             print("Error: %s - %s." % (e.filename, e.strerror))
-    
+            report_issue(" issue with file readers report to developer")
+
         main_directory = "Functional skills maths"
         # Parent Directory path
         parent_directory = "./"
@@ -110,10 +132,10 @@ with open(main_page_path ,"w") as h:
     for part in level1:
         h.write(f'<a href = "./{part}/{part}.html"> go to {part} </a>')
         os.mkdir(f'./{main_directory}/{part}')
-        
+        report_success(f'{part} was found and added to the site build')
 
 banners('banner1.txt','banner2.txt',main_page_path)
-print(level1)
+
 # checking this it is locating the entire directory for this repo
 
 
@@ -121,9 +143,9 @@ print(level1)
 # generate actual lesson content now navigation pages are completed
 for part in level1:
     part_name = f'{part}.html'
-    print(part)
+    #print(part)
     sections = os.listdir( f'{base_path}/{part}')
-    print(f' sections in {part} are {sections}')
+    report_success(f' sections in {part} are {sections} they will be added to site')
 
 
     with open(f'./{main_directory}/{part}/{part_name}', "w") as g:
@@ -148,52 +170,58 @@ for part in level1:
 
             # now lesson page has been established find resource types.
             
-            # find videos and load them
+            # find page plans and generate pages
             try:
                 structure = os.listdir(f'{base_path}/{part}/{section}/instructionsections')
-                print(f'found session structure for {part}{section}')
+                report_success(f'found session structure for {part}{section}')
                 #print(structure)
-                structure = [file for file in structure if file.endswith('csv') ]
+                structure = [file for file in structure if file.endswith('xlsx') ]
                 #print(structure)
-                lesson = pd.read_csv(f'{base_path}/{part}/{section}/instructionsections/{structure[0]}')
-                lesson.dropna()
-                print('pandas worked')
+                location = f'{base_path}/{part}/{section}/instructionsections/{structure[0]}'
+                report_success(f'planning doc is located at {location}')
+                lesson = pd.read_excel(f'{base_path}/{part}/{section}/instructionsections/{structure[0]}')
+                #lesson.dropna()
+                #print('pandas worked')
                 for index, row in lesson.iterrows():
-                    #print('df loaded')
-                    if row['type'] == 'heading' and row['content'].lower()!='nan':
-                        print(row['content'])
-                        f.write(f'<h2>{row["content"]}</h2>')
+                    #print(str(row['Content']).lower())
+                    if row['Type'] == 'heading' and str(row['Content']).lower() !='nan':
+                        #print(row['Content'])
+                        f.write(f'<h2>{row["Content"]}</h2>')
 
-                    elif row['type'] == 'text':
-                        f.write(f'<p>{row["content"]}</p>')
+                    elif row['Type'] == 'paragraph'  and str(row['Content']).lower() !='nan':
+                        #print(row['Content'])
+                        f.write(f'<p>{row["Content"]}</p>')
 #<div style="position: relative; padding-bottom: 56.25%; height: 0;"><iframe src="https://www.loom.com/embed/de00a87a28864b7784607282e65f40dd" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe>
-                    elif row['type']== 'vid':
-                        f.write(f'<iframe class = "vid" src = {row["content"]} webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>')
+                    elif row['Type']== 'video'  and str(row['Content']).lower() !='nan':
+                        #print(row['Content'])
+                        f.write(f'<iframe class = "vid" src = {row["Content"]} webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>')
 
                     else:
-                        print('content type not recognised check your planning document for errors')
+                        report_issue(f'content type not recognised check your planning document for errors - find it at {location}')
             except:
-                print('there appears to be no planning doc for this section')
-
+                report_issue(f'There is an error with a planning doc check in {location} if there should be content for this section')
+                #lesson = pd.read_excel(f'{base_path}/{part}/{section}/instructionsections/{structure[0]}')
 
              
             # find pdfs of exercises
 
-            f.write(f'<h2> Practice your skills using our exercises</h2>')
+            
             try:
                 pdf_path = f'{base_path}/{part}/{section}/pdfs'
                 # content\part_1\section_1\exercises\T01 Class Ex - Directed Numbers.pdf
-                print(f'pdfs are stored in {pdf_path}')
+                report_success(f'pdfs were found in {pdf_path}')
                 #pp = os.listdir(f'{base_path}/{section}')
                 pp = os.listdir(f'{base_path}/{part}/{section}/pdfs')
-                print(pp)
-                for pdf in pp:
-                    print(pdf)
-                    f.write(f'<iframe class="pdf coached" src="../../content/{part}/{section}/pdfs/{pdf}"  type="application/pdf" allow="fullscreen"></iframe>\n')
-                #for pdf in pdfs:
+                if pp:
+                    f.write(f'<h2> Practice your skills using our exercises</h2>')
+                    #print(pp)
+                    for pdf in pp:
+                        print(pdf)
+                        f.write(f'<iframe class="pdf coached" src="../../content/{part}/{section}/pdfs/{pdf}"  type="application/pdf" allow="fullscreen"></iframe>\n')
+                    #for pdf in pdfs:
                 #f.write(f'<embed class="pdf coached" src="{pdf}" type="application/pdf">')
             except:
-                print(f'no exercises folder present in {section}')
+                report_issue(f'no pdf exercises folder present in {pdf_path} check if this is needed')
 
             #find pdfs of exams
 
@@ -202,19 +230,19 @@ for part in level1:
             try:
                 pdf_path = f'{base_path}/{part}/{section}/examqs'
                 # content\part_1\section_1\exercises\T01 Class Ex - Directed Numbers.pdf
-                print(f'pdfs are stored in {pdf_path}')
+                report_success(f'exam pdfs were found in {pdf_path}')
                 #pp = os.listdir(f'{base_path}/{section}')
                 pp = os.listdir(f'{base_path}/{part}/{section}/examqs')
-                print(pp)
+                #print(pp)
                 for pdf in pp:
-                    print(pdf)
+                    #print(pdf)
                     f.write(f'<iframe class="pdf coached" src="../../content/{part}/{section}/examqs/{pdf}"  type="application/pdf" allow="fullscreen"></iframe>\n')
                 #for pdf in pdfs:
                 #f.write(f'<embed class="pdf coached" src="{pdf}" type="application/pdf">')
             except:
-                print(f'no exam folder present in {section}')
+                report_issue(f'no exam folder present in {pdf_path}')
 
-
+print(f'full process ran with {issue_count} reports in error report' )
 
 
 
